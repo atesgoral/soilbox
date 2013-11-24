a35010211c7d763466eb1093b83d2a6378937253
Ates Goral <ates@magnetiq.com>
2013-11-24 17:49:50 -0500

    Removed socket.io, added static fiel serving

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
+app.use("/", express.static(__dirname));
+
+/*app.get('/', function(request, response) {
     response.send('Hello World!');
 });
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
@@ -15,7 +18,7 @@ var mongo = require('mongodb');
 
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
+  'mongodb://localhost/soilbox';
 
 mongo.Db.connect(mongoUri, function (err, db) {
   db.collection('mydocs', function(er, collection) {
