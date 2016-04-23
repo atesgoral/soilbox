07005b811989fdc795c5b029f7c9c4e99e15a490
Ates Goral <ates.g@myplanet.com>
2016-04-23 17:51:55 -0400

    Removed mongodb

--- web.js
+++ web.js
@@ -1,28 +1,10 @@
 var express = require("express");
 var app = express();
+
 app.use(express.logger());
 
 app.use("/", express.static(__dirname));
 
-/*app.get('/', function(request, response) {
-    response.send('Hello World!');
-});
-*/
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
-
-var mongo = require('mongodb');
-
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/soilbox';
-
-mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });
+var server = app.listen(process.env.PORT || 5000, function() {
+    console.log("Listening on " + server.address().port);
 });
